4090b641bc1fdf2a6174c5a10a25dc27641cb1ed
Marino Faggiana <ios@nextcloud.com>
2017-04-27 15:11:50 +0200

    New version

---
+++ Custom/Clarosync/NCBrandConstant.h
@@ -0,0 +1,48 @@
+//
+//  NCBrandConstant.h
+//  Nextcloud
+//
+//  Created by Marino Faggiana on 06/03/17.
+//  Copyright © 2017 TWS. All rights reserved.
+//
+//  Author Marino Faggiana <m.faggiana@twsweb.it>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// Brand Text
+#define k_brand                                 @"ClaroSync"
+#define k_mailMe                                @"info@clarosync.com"
+#define k_textCopyrightNextcloudiOS             @"ClaroSync for iOS %@ © 2017 T.W.S. Inc."
+#define k_textCopyrightNextcloudServer          @"ClaroSync Server %@"
+#define k_loginBaseUrl                          @"https://skylake.weasel.rocks/c/mexico/clarovideo"
+#define k_pushNotificationServer                @"https://push-notifications.nextcloud.com"
+#define k_loginButtonLabelLink                  @"https://nextcloud.com/providers"
+#define k_webLoginAutenticationProtocol         @"amx://"
+
+// Capabilities Group
+#define k_capabilitiesGroups                    @"group.it.twsweb.Clarosync"
+
+// Option 
+#define k_option_use_login_web                  YES
+#define k_option_use_firebase                   YES
+#define k_option_use_default_automatic_upload   YES
+#define k_option_use_themingColor               YES
+#define k_option_use_themingBackground          YES
+
+#define k_option_disable_intro                  YES
+#define k_option_disable_request_login_url      NO
+#define k_option_disable_multiaccount           YES
+#define k_option_disable_cryptocloudsystem      YES
+
